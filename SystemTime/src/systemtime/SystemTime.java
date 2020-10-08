0e84499e75a723aaafc39062e9da3a7e0372ade6
Fernando Silva <fernnlcs@gmail.com>
2020-10-08 20:01:54 -0300

    SystemTime

---
+++ SystemTime/src/systemtime/SystemTime.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package systemtime;
+
+import java.util.Date;
+
+/**
+ *
+ * @author temp
+ */
+public class SystemTime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.print("Current date and time: ");
+        System.out.println(now.toString());
+    }
+    
+}
